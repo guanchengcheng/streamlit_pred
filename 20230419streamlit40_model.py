ca65764bc7ca3ca43c8a9574af58f2af03ab0d70
HanMuYa <45205738+HanMuYa@users.noreply.github.com>
2023-04-21 14:06:05 +0800

    Update 20230419streamlit40_model.py

--- 20230419streamlit40_model.py
+++ 20230419streamlit40_model.py
@@ -124,7 +124,7 @@ if raw_data:
         st.dataframe(disp, use_container_width=True)
 
     # 训练数据文件
-    df1=  pd.read_csv(file[0], encoding="ANSI")
+    df1=  pd.read_csv(file[0], encoding="gbk")
 
     with st.expander("**Train data sets**", True):
         st.dataframe(df1, use_container_width=True)
